f90b14caaea5354f699226b56404ee44d3dc2756
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2022-12-29 15:32:24 +0300

    düzenleme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,6 @@ module.exports = {
         'poppins': ['Poppins', 'sans-serif'],
         'petit': ['Petit Formal Script', 'sans-serif'],
       },
-      animation: {
-        wiggle: 'wiggle 3s ease-in-out infinite'
-      },
-      keyframes: {
-        wiggle: {
-          '0%, 100%': { transform: 'rotate(-5deg)' },
-          '50%': { transform: 'rotate(5deg)' }
-        },
-      },
     },
   },
   plugins: [],
